bc8b4c56446ea097e421d03c5717810b8e52310c
victoriasilvina <victoriasilvina36@gmail.com>
2024-09-20 04:01:26 -0300

    models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Pessoal(models.Model):
+    titulo = models.CharField(max_length=100)
+    subtitulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    imagem = models.ImageField(upload_to='/images/', blank=True)
+    
+    def __str__(self):
+        return self.titulo
+
+class Rotina(models.Model):
+    titulo = models.CharField(max_length=100)
+    subtitulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    imagem = models.ImageField(upload_to='/images/', blank=True)
+    
+    def __str__(self):
+        return self.titulo
+    
+class Gostos(models.Model):
+    titulo = models.CharField(max_length=100)
+    subtitulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    imagem = models.ImageField(upload_to='/images/', blank=True)
+    
+    def __str__(self):
+        return self.titulo
+
+
+    
