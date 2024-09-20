b91f3e1e60247dfa3dd999bdda9e6a7e3e618e0f
victoriasilvina <victoriasilvina36@gmail.com>
2024-09-20 04:12:29 -0300

    models

--- blog/models.py
+++ blog/models.py
@@ -6,25 +6,21 @@ class Pessoal(models.Model):
     titulo = models.CharField(max_length=100)
     subtitulo = models.CharField(max_length=100)
     descricao = models.TextField()
-    imagem = models.ImageField(upload_to='/images/', blank=True)
+    imagem = models.ImageField(upload_to='images/', blank=True)
     
     def __str__(self):
         return self.titulo
 
 class Rotina(models.Model):
     titulo = models.CharField(max_length=100)
-    subtitulo = models.CharField(max_length=100)
     descricao = models.TextField()
-    imagem = models.ImageField(upload_to='/images/', blank=True)
     
     def __str__(self):
         return self.titulo
     
 class Gostos(models.Model):
     titulo = models.CharField(max_length=100)
-    subtitulo = models.CharField(max_length=100)
     descricao = models.TextField()
-    imagem = models.ImageField(upload_to='/images/', blank=True)
     
     def __str__(self):
         return self.titulo
